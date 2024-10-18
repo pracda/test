96408929f0f5e9acf55e9569900fa560fd44a3c3
Prasiddha Paudel <pracda.paudel@gmail.com>
2024-10-18 16:21:34 -0500

    hey

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+exports.handler = async (event) => {
+    console.log("Event: ", event);
+    return {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+};
